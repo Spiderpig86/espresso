7b6dd6942cec7650c5bbb67b6accf94dcd9f8b17
Stan <slim679975@gmail.com>
2018-06-16 22:24:20 -0400

    feat(Program.cs): Completed main start file

---
+++ Espresso/EspressoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Espresso {
+
+    /// <summary>
+    ///     Keeps track of app state and persistence
+    /// </summary>
+    public class EspressoContext : ApplicationContext {
+
+    }
+}
--- Espresso/Program.cs
+++ Espresso/Program.cs
@@ -1,17 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Espresso {
     static class Program {
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Only allow for one instance of this program
+            bool hasBeenCreated = false;
+            String mutexName = System.Reflection.Assembly.GetExecutingAssembly().GetType().GUID.ToString(); // Get the GUID of the app
+
+            // Only spawn a single thread
+            using (Mutex mutex = new Mutex(false, mutexName, out hasBeenCreated)) {
+                // hasBeenCreated will be assigned true after lock is created
+                if (!hasBeenCreated) {
+                    // Only allow one instance
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                try {
+                    EspressoContext context = new EspressoContext();
+                    Application.Run(context);
+                } catch (Exception e) {
+                    MessageBox.Show(e.Message, "Espresso Startup Error");
+                }
+            }
+
         }
     }
 }
